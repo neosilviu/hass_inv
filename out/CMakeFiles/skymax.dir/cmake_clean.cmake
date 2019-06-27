f9baa69391d1eaa7d3799ef625a6087a1983e754
neosilviu <neosilviu@gmail.com>
2019-06-27 23:51:10 +0300

    Add files via upload

---
+++ out/CMakeFiles/skymax.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/skymax.dir/inputparser.cpp.o"
+  "CMakeFiles/skymax.dir/main.cpp.o"
+  "CMakeFiles/skymax.dir/skymax.cpp.o"
+  "CMakeFiles/skymax.dir/tools.cpp.o"
+  "skymax.pdb"
+  "skymax"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/skymax.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
